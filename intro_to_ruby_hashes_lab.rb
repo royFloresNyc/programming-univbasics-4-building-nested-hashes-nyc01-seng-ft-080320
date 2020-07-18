160bb8eaf0b7144643ef43ca6772b9fb8caf6fa5
Roy  Flores <royfloresnyc@gmail.com>
2020-07-18 19:50:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
     :three_pieces_owned = 100,
     :four_pieces_owned = 200
   }
-  new_hash[:railroads][:names][:reading_railroads] = {}
+  new_hash[:railroads][:names][:reading_railroad] = {}
   new_hash
 end
 
