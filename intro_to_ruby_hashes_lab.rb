fb660b4e9a674c77c6a51d906d60a1bbbe3bdf2d
Roy  Flores <royfloresnyc@gmail.com>
2020-07-18 19:49:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,13 @@ def monopoly_with_third_tier
   # complexity.
   
   new_hash = monopoly_with_second_tier
-  new_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-  new_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
-  new_hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
-  new_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  new_hash[:railroads][:rent_in_dollars] = {
+    :one_piece_owned = 25,
+    :two_pieces_owned = 50,
+    :three_pieces_owned = 100,
+    :four_pieces_owned = 200
+  }
+  new_hash[:railroads][:names][:reading_railroads] = {}
   new_hash
 end
 
