d3e4717cc5b973c8211119fec52e1fcf5a6104b0
Roy  Flores <royfloresnyc@gmail.com>
2020-07-18 19:36:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,6 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   new_hash = {:railroads => {}}
+  new_hash[:railroads][:pieces] = 4
 end
 
 def monopoly_with_third_tier
